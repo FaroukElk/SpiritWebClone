3f8a6ef32ac62493765977ea739277feffa030e9
FaroukElk <faroukelkhayat@outlook.com>
2018-01-17 12:49:38 -0500

    Initial commit

--- src/main/java/spirit/Hotel.java
+++ src/main/java/spirit/Hotel.java
@@ -4,10 +4,52 @@ import java.util.Date;
 
 public class Hotel {
 
-	private String location;
-	private Date checkin;
-	private Date checkout;
+	private String pickupLocation;
+	private Date checkinDate;
+	private Date checkoutDate;
 	private int adults;
 	private int children;
+	
+	public String getPickupLocation() {
+		return pickupLocation;
+	}
+	
+	public void setPickupLocation(String pickupLocation) {
+		this.pickupLocation = pickupLocation;
+	}
+	
+	public Date getCheckinDate() {
+		return checkinDate;
+	}
+	
+	public void setCheckinDate(Date checkinDate) {
+		this.checkinDate = checkinDate;
+	}
+	
+	public Date getCheckoutDate() {
+		return checkoutDate;
+	}
+	
+	public void setCheckoutDate(Date checkoutDate) {
+		this.checkoutDate = checkoutDate;
+	}
+	
+	
+	public int getAdults() {
+		return adults;
+	}
+	
+	public void setAdults(int adults) {
+		this.adults = adults;
+	}
+	
+	public int getChildren() {
+		return children;
+	}
+	
+	public void setChildren(int children) {
+		this.children = children;
+	}
+	
 	
 }
--- src/main/java/spirit/Traveler.java
+++ src/main/java/spirit/Traveler.java
@@ -8,5 +8,28 @@ public class Traveler{
 	
 	private String confirmCode;
 	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getConfirmCode() {
+		return confirmCode;
+	}
+	
+	public void setConfirmCode(String confirmCode) {
+		this.confirmCode = confirmCode;
+	}
 	
 }
